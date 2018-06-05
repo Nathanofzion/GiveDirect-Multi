ec6ce7a9b871ce78add6830cb2c1341c786a296d
fritz <>
2018-06-04 22:16:02 -0400

    testnet and remotely verify if account is funded or not

--- js/main.js
+++ js/main.js
@@ -1,20 +1,47 @@
 givedirect = {};
-givedirect.horizonServer = "https://horizon.stellar.org";
-givedirect.interstellarMultisigApi = "https://interstellar.exchange/backend/api/v1/transaction_envelope";
 
-givedirect.processForm = function (source, destinations, amount, memo, submitButton) {
+
+givedirect.processForm = function (source, destinations, amount, memo, network, submitButton) {
+
 
     var that = this;
+    givedirect.operations = [];
     destinations = destinations.trim();
+
 
     var progressReport = $("#progressReport");
     progressReport.html("");
 
     progressReport.append("<strong>Progress Report</strong><br>");
 
+    progressReport.append("Checking Network <br>");
+
+    if (!network) {
+        progressReport.append("Select either testnet or mainnet.<br> Aborting.");
+        submitButton.show();
+        return;
+    }
+
+    var isTestnet = true;
+    var horizonServer = "https://horizon-testnet.stellar.org";
+    var interstellarMultisigApi = "https://testnet.interstellar.exchange/backend/api/v1/transaction_envelope";
+
+    if (network == "mainnet") {
+        isTestnet = false;
+        horizonServer = "https://horizon.stellar.org";
+        interstellarMultisigApi = "https://interstellar.exchange/backend/api/v1/transaction_envelope";
+    }
+
+    if (isTestnet) {
+        progressReport.append("TESTNET selected.<br>");
+    }
+    else {
+        progressReport.append("MAINNET selected.<br>");
+    }
+
     progressReport.append("Checking Memo Length <br>");
 
-    if(memo.length > 26){
+    if (memo.length > 26) {
         progressReport.append("Memo length: " + memo.length + " &gt 26.<br> Aborting.");
         submitButton.show();
         return;
@@ -36,28 +63,22 @@ givedirect.processForm = function (source, destinations, amount, memo, submitBut
     }
 
     var invalidDestinations = [];
-    var destinationArray = destinations.split("\n");
-    var operations = [];
-    var asset = StellarSdk.Asset.native();
 
-    for (i = 0; i < destinationArray.length; i++) {
+    var destinationArray = destinations.split("\n");
+
+
+    //check locally for valid destination accounts.
+
+    for (var i = 0; i < destinationArray.length; i++) {
 
         var destinationToTest = destinationArray[i];
-        progressReport.append((i + 1) + ": Checking Stellar Destination Address: " + destinationToTest + "<br>");
+        progressReport.append((i + 1) + ": Locally Checking Stellar Destination Address: " + destinationToTest + "<br>");
 
         try {
             var dummy = StellarSdk.Keypair.fromPublicKey(destinationToTest);
-
-            var operation = StellarSdk.Operation.payment({
-                destination: destinationToTest,
-                asset: asset,
-                amount: '' + amount,
-            });
-
-            operations.push(operation);
-
         }
         catch (err) {
+            console.error(err);
             invalidDestinations.push(destinationToTest);
         }
     }
@@ -65,7 +86,7 @@ givedirect.processForm = function (source, destinations, amount, memo, submitBut
     if (invalidDestinations.length > 0) {
         progressReport.append("Error: The following destination addresses are invalid: <br> <ul>");
 
-        for (i = 0; i < invalidDestinations.length; i++) {
+        for (var i = 0; i < invalidDestinations.length; i++) {
             var invalidDestination = invalidDestinations[i];
             progressReport.append("<li>" + invalidDestination + "</li>");
         }
@@ -76,15 +97,25 @@ givedirect.processForm = function (source, destinations, amount, memo, submitBut
 
     }
 
-    progressReport.append("All " + destinationArray.length + " destination addresses are valid. Getting updated info for the source account <br> ");
+
+    progressReport.append("All " + destinationArray.length + " destination addresses are valid. <br> ");
     progressReport.append("... <br> ");
-    progressReport.append("Please be patient while we get the latest source account information from the Internet<br> ");
+    progressReport.append("Please be patient while we get the latest destination accounts info from the Internet<br> ");
     progressReport.append("... <br> ");
     progressReport.append("... <br> ");
 
-    var server = new StellarSdk.Server(givedirect.horizonServer);
+    var server = new StellarSdk.Server(horizonServer);
 
-    server.loadAccount(source)
+    givedirect.buildOperations(progressReport, server, destinationArray, amount)
+        .then(function (operations) {
+            console.log(operations);
+            progressReport.append("All " + destinationArray.length + " operations have been created. Getting updated info for the source account <br> ");
+            progressReport.append("... <br> ");
+            progressReport.append("Please be patient while we get the latest source account information from the Internet<br> ");
+            progressReport.append("... <br> ");
+            progressReport.append("... <br> ");
+            return server.loadAccount(source);
+        })
         .then(function (account) {
 
             if (account.signers.length < 2) {
@@ -100,8 +131,8 @@ givedirect.processForm = function (source, destinations, amount, memo, submitBut
 
             builder = builder.addMemo(StellarSdk.Memo.text(memo));
 
-            for (i = 0; i < operations.length; i++) {
-                var operation = operations[i];
+            for (i = 0; i < givedirect.operations.length; i++) {
+                var operation = givedirect.operations[i];
                 builder.addOperation(operation);
             }
 
@@ -116,7 +147,7 @@ givedirect.processForm = function (source, destinations, amount, memo, submitBut
             progressReport.append("... <br> ");
 
             $.ajax({
-                url: givedirect.interstellarMultisigApi,
+                url: interstellarMultisigApi,
                 type: 'post',
                 dataType: 'text',
                 data: xdr,
@@ -125,16 +156,100 @@ givedirect.processForm = function (source, destinations, amount, memo, submitBut
                     progressReport.append("Successfully submitted transaction to Interstellar's Multisig Service <br> ");
                 }
             });
+
         })
-        .catch(function (e) {
-            console.error(e);
-            progressReport.append("There was a network error. Aborting.");
+
+        .catch(function (err) {
+
+            progressReport.append("There was an error <br> ");
+            progressReport.append(JSON.stringify(err) + " <br> ");
+            progressReport.append("</ul>Aborting");
             submitButton.show();
+            return Promise.reject([]);
+
+        })
+
+
+}
+
+
+givedirect.buildOperations = function (progressReport, server, destinations, amount) {
+
+    return new Promise(function (resolve, reject) {
+
+        givedirect.operations = [];
+
+        givedirect.internalBuildOperations(progressReport, server, destinations, amount, 0, resolve, reject);
+
+
+    });
+
+
+}
+
+givedirect.internalBuildOperations = function (progressReport, server, destinations, amount, index, resolve, reject) {
+
+
+
+    if (givedirect.operations.length == destinations.length) {
+        //success.
+        resolve(givedirect.operations);
+        return;
+    }
+
+    if (index >= destinations.length) {
+        reject("unknown error");
+        return;
+    }
+
+    var asset = StellarSdk.Asset.native();
+
+
+    var destination = destinations[index];
+    progressReport.append((index + 1) + ": Remotely Checking Stellar Destination Address: " + destination + "<br>");
+
+    //check remotely whether account is already funded or not.
+
+    server.loadAccount(destination)
+        .then(function (account) {
+
+            var operation = StellarSdk.Operation.payment({
+                destination: destination,
+                asset: asset,
+                amount: '' + amount
+            });
+
+            givedirect.operations.push(operation);
+            progressReport.append((givedirect.operations.length) + ": (FUNDED_ACCOUNT) " + destination + "<br>");
+
+            givedirect.internalBuildOperations(progressReport, server, destinations, amount, index + 1, resolve, reject);
             return;
 
         })
+        .catch(function (err) {
+            if (err && err.name && err.name == "NotFoundError") {
+                var operation = StellarSdk.Operation.createAccount({
+                    destination: destination,
+                    startingBalance: '' + amount
+                });
+                givedirect.operations.push(operation);
+                progressReport.append((givedirect.operations.length) + ": (UNFUNDED_ACCOUNT) " + destination + "<br>");
+                givedirect.internalBuildOperations(progressReport, server, destinations, amount, index + 1, resolve, reject);
+                return;
+
+            }
+            else {
+                progressReport.append("There was an error getting the status of " + destination + " <br> ");
+                console.error(err);
+                reject(err);
+                return;
+            }
+
+        });
 
 }
+
+
 
 $(document).ready(function () {
 
@@ -142,16 +257,17 @@ $(document).ready(function () {
     console.log(window);
 
     var form = $("#form");
-    var submitButton = $("#submitButton");
-    var inputSource = $("#source");
-    var destinations = $("#destinations");
-    var memo = $("#memo");
-    var amount = $("#amount");
 
     form.on("submit", function (event) {
+        var submitButton = $("#submitButton");
+        var source = $("#source").val();
+        var destinations = $("#destinations").val();
+        var memo = $("#memo").val();
+        var amount = $("#amount").val();
+        var network = $("input[name='network']:checked").val();
         event.preventDefault();
         submitButton.hide();
-        givedirect.processForm(inputSource.val(), destinations.val(), amount.val(), memo.val(), submitButton);
+        givedirect.processForm(source, destinations, amount, memo, network, submitButton);
     });
 
 });
